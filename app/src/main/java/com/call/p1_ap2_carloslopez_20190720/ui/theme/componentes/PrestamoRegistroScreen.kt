40e2c3ad0f917aa4a5eb869fe404ab651866d29d
carlos lopez <88679267+carlos25u@users.noreply.github.com>
2022-06-08 19:15:23 -0400

    mejorando diseño de registro de Prestamos

--- app/src/main/java/com/call/p1_ap2_carloslopez_20190720/ui/theme/componentes/PrestamoRegistroScreen.kt
+++ app/src/main/java/com/call/p1_ap2_carloslopez_20190720/ui/theme/componentes/PrestamoRegistroScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.SaveAs
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -12,10 +15,23 @@ import com.call.p1_ap2_carloslopez_20190720.view.PrestamoViewModel
 
 @Composable
 fun PrestamoRegistroScreen(backToListado:() -> Unit, viewModel: PrestamoViewModel = hiltViewModel()){
+    val ScaffoldState = rememberScaffoldState()
     Scaffold(
         topBar = {
             TopAppBar(title = { Text(text = "Registro de Prestamos") })
-        }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    viewModel.Guardar()
+                    backToListado()
+                },
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(imageVector = Icons.Default.SaveAs, contentDescription = "Guardar")
+            }
+        },
+        scaffoldState = ScaffoldState
     ) {it
         Column(
             modifier = Modifier
@@ -42,17 +58,6 @@ fun PrestamoRegistroScreen(backToListado:() -> Unit, viewModel: PrestamoViewMode
                 label = { Text(text = "Monto")},
                 modifier = Modifier.fillMaxWidth()
             )
-
-            Button(onClick = {
-                viewModel.Guardar()
-                backToListado()
-            },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 20.dp)
-            ) {
-                Text(text = "Guardar")
-            }
         }
     }
 }
