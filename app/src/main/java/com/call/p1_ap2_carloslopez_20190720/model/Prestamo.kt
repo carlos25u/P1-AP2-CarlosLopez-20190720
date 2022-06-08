9e769989b883264c1be0de180c2b967d5b0c222a
carlos lopez <88679267+carlos25u@users.noreply.github.com>
2022-06-08 18:10:07 -0400

    haciendo entidad Prestamo

--- app/src/main/java/com/call/p1_ap2_carloslopez_20190720/model/Prestamo.kt
+++ app/src/main/java/com/call/p1_ap2_carloslopez_20190720/model/Prestamo.kt
@@ -3,8 +3,11 @@ package com.call.p1_ap2_carloslopez_20190720.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Clientes")
+@Entity(tableName = "Prestamos")
 data class Cliente(
     @PrimaryKey(autoGenerate = true)
-    val clienteId: Int=0,
+    val prestamoId: Int=0,
+    val deudor: String,
+    val concepto: String,
+    val monto: Int = 0
 )
