1570a41d13228436d21bb1a899b595b775738194
carlos lopez <88679267+carlos25u@users.noreply.github.com>
2022-06-08 18:45:17 -0400

    haciendo PrestamoRegistroScreen

--- app/src/main/java/com/call/p1_ap2_carloslopez_20190720/ui/theme/componentes/PrestamoRegistroScreen.kt
+++ app/src/main/java/com/call/p1_ap2_carloslopez_20190720/ui/theme/componentes/PrestamoRegistroScreen.kt
@@ -3,10 +3,7 @@ package com.call.p1_ap2_carloslopez_20190720.ui.theme.componentes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,13 +14,34 @@ import com.call.p1_ap2_carloslopez_20190720.view.PrestamoViewModel
 fun PrestamoRegistroScreen(backToListado:() -> Unit, viewModel: PrestamoViewModel = hiltViewModel()){
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "Registro") })
+            TopAppBar(title = { Text(text = "Registro de Prestamos") })
         }
     ) {it
         Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp)) {
+
+            OutlinedTextField(
+                value = viewModel.deudor,
+                onValueChange = {viewModel.deudor = it},
+                label = { Text(text = "Deudor")},
+                modifier = Modifier.fillMaxWidth()
+                )
+
+            OutlinedTextField(
+                value = viewModel.concepto,
+                onValueChange = {viewModel.concepto = it},
+                label = { Text(text = "Concepto")},
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = viewModel.monto,
+                onValueChange = {viewModel.monto = it},
+                label = { Text(text = "Monto")},
+                modifier = Modifier.fillMaxWidth()
+            )
 
             Button(onClick = {
                              backToListado()
